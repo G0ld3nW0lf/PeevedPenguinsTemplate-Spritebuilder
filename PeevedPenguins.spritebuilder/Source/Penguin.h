e074d49c093f1d4423d78c06fe46b675e22a535b
Shashank Guduru <shashank.guduru@gmail.com>
2014-06-25 10:51:38 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Penguin.h
@@ -0,0 +1,15 @@
+//
+//  Penguin.h
+//  PeevedPenguins
+//
+//  Created by Shashank Guduru on 6/25/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Penguin : CCSprite
+
+-(id)init;
+
+@end
