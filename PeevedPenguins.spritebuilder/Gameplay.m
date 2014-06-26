b68da10491fdc41c24b415697c9a21868871ba72
Shashank Guduru <shashank.guduru@gmail.com>
2014-06-26 14:51:43 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -12,6 +12,7 @@
     CCPhysicsNode *_physicsNode;
     CCNode *_levelNode;
     CCNode *_catapultArm;
+    CCNode *_contentNode;
 }
 
 -(void)didLoadFromCCB{
@@ -40,7 +41,7 @@
     
     self.position = ccp(0, 0);
     CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
-    [self runAction:follow];
+    [_contentNode runAction:follow];
 }
 
 -(void)retry{
