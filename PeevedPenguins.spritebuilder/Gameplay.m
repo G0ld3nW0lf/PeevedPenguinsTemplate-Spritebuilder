af963c7988d5c145cd36b6af6de68a3d08c2fab1
Shashank Guduru <shashank.guduru@gmail.com>
2014-06-25 21:17:59 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -28,7 +28,7 @@
     [_physicsNode addChild:penguin];
     
     CGPoint launchDirection = ccp(1, 0);
-    CGPoint force = ccpMult(launchDirection, 8000);
+    CGPoint force = ccpMult(launchDirection, 6000);
     [penguin.physicsBody applyForce:force];
 }
 
