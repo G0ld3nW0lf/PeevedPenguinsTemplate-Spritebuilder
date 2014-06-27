cc796661f1d7591d8226165badd8989fdcb3a7cd
Shashank Guduru <shashank.guduru@gmail.com>
2014-06-27 00:00:47 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -31,7 +31,7 @@
 
 -(void)launchPenguin{
     CCNode *penguin = [CCBReader load:@"Penguin"];
-    penguin.position = ccpAdd(_catapultArm.position, ccp(42,100));
+    penguin.position = ccpAdd(_catapultArm.position, ccp(42,90));
     
     [_physicsNode addChild:penguin];
     
