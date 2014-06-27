9fe0a0247ab1a316679e7ccb7fccfccc42e35708
Shashank Guduru <shashank.guduru@gmail.com>
2014-06-27 11:10:31 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -68,8 +68,8 @@
 {
     // when touches end, meaning the user releases their finger, release the catapult
     [self releaseCatapult];
+    _mouseJointNode.position = ccp(0,0);
 }
-
 -(void) touchCancelled:(UITouch *)touch withEvent:(UIEvent *)event
 {
     // when touches are cancelled, meaning the user drags their finger off the screen or onto something else, release the catapult
